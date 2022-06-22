7d6125b230a4c089de6b53260782b12d75a2b410
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2022-06-21 21:06:25 -0300

    configurando blade no lumen

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class Home extends Controller
 {
-    public function index(){
-        return view('site.home',['title'=>'home', 'name'=>'Savio']);
+    public function index()
+    {
+        return view('site.home', ['title' => 'home', 'name' => 'Savio']);
     }
 }
